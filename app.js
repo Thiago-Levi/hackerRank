b10f966d266a6ece90c8b0354c019b347130b6f4
Thiago-Levi <thiago.levi@outlook.com>
2023-05-04 09:43:30 -0300

    add

---
+++ app.js
@@ -0,0 +1,13 @@
+const arrayOfIntegers = [
+  1000000001, 1000000002, 1000000003, 1000000004, 1000000005,
+];
+
+function aVeryBigSum(arrayOfIntegers) {
+  const sumResult = arrayOfIntegers.reduce(
+    (acc, aInteger) => (acc += aInteger),
+    0
+  );
+  return sumResult;
+}
+
+console.log(aVeryBigSum(arrayOfIntegers));
